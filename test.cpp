c1442ff25d8e1b5340915a49d77fd74144006e13
rvsatyam <62536472+rvsatyam@users.noreply.github.com>
2022-06-23 17:01:33 +0530

    First commit
    Working on operator /=

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include "bigint.h"
+int main(){
+    string s = "99";
+    BigInt a(25), b(-9);
+    cout << a << endl;
+    cout << b << endl;
+    // cout << a * b << endl;
+    cout << a / b << endl;
+    // cout << b / a << endl;
+    // cout << a << endl;
+    // cout << b << endl;
+    return 0;
+}
